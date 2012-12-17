6f3c747205eabac4089dda81196fb5a7b4b8f49f
Bright_Pan <loststriker@gmail.com>
2012-12-17 12:10:09 +0800

    Two commit
    Change-Id: I69549ac231dfdec715eeb04eaec7396aab4d5065
    Signed-off-by: Bright_Pan <loststriker@gmail.com>

---
+++ b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+Created on 2012-12-17
+
+@author: ThinkPad
+'''
+print '我的家里'
---
+++ c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2012-12-17
+
+@author: Bright Pan
+@email: loststriker@gmail.com
+'''
+
+print 1234
